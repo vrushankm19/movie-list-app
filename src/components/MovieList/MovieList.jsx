4e40a2f8bbbe88284b683a6eea6c60cf225c9aca
Vrushank <vrushank.m@amuratech.com>
2024-08-08 14:04:39 +0530

    final 1

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,108 +1,87 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Loader from '../Features/Loader';
 import './MovieList.scss';
 
 const MovieList = ({ selectedGenres }) => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState({});
+  const [currentYear, setCurrentYear] = useState(2012);
+  const [years, setYears] = useState([2012]);
+  const [hasMore, setHasMore] = useState(true);
 
-  const totalPages = 10;
-  const yearRefs = useRef({});
-  const hasScrolled = useRef(false);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      let allMovies = [];
-      setLoading(true);
-
-      try {
-        // Loop to fetch multiple pages of movies
-        for (let page = 1; page <= totalPages; page++) {
-          const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
-            params: {
-              api_key: '382ed57d2aff6607fcb9a6857dbdef40',
-              sort_by: 'popularity.desc',
-              include_adult: false,
-              include_video: false,
-              page: page,
-              with_genres: selectedGenres.length > 0 ? selectedGenres.join(',') : undefined
-            }
-          });
-          allMovies = allMovies.concat(response.data.results);
-          await new Promise(resolve => setTimeout(resolve, 300));
+  const fetchMoviesForYear = useCallback(async (year) => {
+    try {
+      const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+        params: {
+          api_key: '382ed57d2aff6607fcb9a6857dbdef40',
+          sort_by: 'popularity.desc',
+          primary_release_year: year,
+          vote_count_gte: 100,
+          page: 1,
+          with_genres: selectedGenres.length > 0 ? selectedGenres.join(',') : undefined
         }
+      });
 
-        setMovies(allMovies);
-      } catch (error) {
-        console.error('Error fetching data from TMDB:', error);
-      } finally {
-        setLoading(false); // Set loading state to false after fetching
-      }
-    };
-
-    fetchMovies();
+      const newMovies = response.data.results.slice(0, 20);
+      setMovies((prevMovies) => ({
+        ...prevMovies,
+        [year]: newMovies
+      }));
+    } catch (error) {
+      console.error('Error fetching data from TMDB:', error);
+    }
   }, [selectedGenres]);
 
   useEffect(() => {
-    const scrollToYear = () => {
-      const targetYear = 2012;
-      console.log('Checking scroll conditions', { loading, hasScrolled: hasScrolled.current, yearRef: yearRefs.current[targetYear] });
-      if (yearRefs.current[targetYear] && !hasScrolled.current) {
-        yearRefs.current[targetYear].scrollIntoView({ behavior: 'smooth' });
-        hasScrolled.current = true;
-        console.log('Scrolled to 2012');
-      }
-    };
+    fetchMoviesForYear(currentYear);
+  }, [fetchMoviesForYear, currentYear]);
 
-    if (!loading && movies.length > 0) {
-      scrollToYear();
+  const fetchMoreData = () => {
+    const nextYear = currentYear + 1;
+    const prevYear = currentYear - 1;
+    if (nextYear <= new Date().getFullYear()) {
+      setCurrentYear(nextYear);
+      fetchMoviesForYear(nextYear);
+      setYears((prevYears) => [...prevYears, nextYear]);
+    } else if (prevYear > 1900) {
+      setCurrentYear(prevYear);
+      fetchMoviesForYear(prevYear);
+      setYears((prevYears) => [prevYear, ...prevYears]);
+    } else {
+      setHasMore(false);
     }
-  }, [loading, movies]);
-
-  const groupMoviesByYear = (movies) => {
-    return movies.reduce((acc, movie) => {
-      const year = new Date(movie.release_date).getFullYear();
-      if (!acc[year]) {
-        acc[year] = [];
-      }
-      if (acc[year].length < 20) {
-        acc[year].push(movie);
-      }
-      return acc;
-    }, {});
   };
-
-  const groupedMovies = groupMoviesByYear(movies); // Group movies by year
 
   return (
     <div className='tmdb-body'>
       <div className='container tmdb-body-con'>
-        <div className='tmdb-body-data'>
-          {
-            loading 
-            ? <Loader />
-            : (
-              Object.keys(groupedMovies).sort((a, b) => a - b).map(year => (
-                <div key={year} ref={el => yearRefs.current[year] = el}>
-                  <h2 className='tmdb-body-title'>{year}</h2>
-                  <div className='tmdb-body-flex'>
-                    {groupedMovies[year].sort((a, b) => b.popularity - a.popularity).map(item => (
-                      <div key={item.id} className='tmdb-body-flex-item'>
-                        <img className='tmdb-body-flex-item-img' src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.original_title} />
-                        <div className='tmdb-body-flex-item-copy'>
-                          <h3 className='tmdb-body-flex-item-copy-h3'>{item.original_title}</h3>
-                          <p className='tmdb-body-flex-item-copy-p'>{item.overview}</p>
-                        </div>
-                          <p className='tmdb-body-flex-item-copy-tag'>{item.vote_count}</p>
-                      </div>
-                    ))}
+        <InfiniteScroll
+          dataLength={years.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<Loader />}
+          scrollThreshold={0.9} // Load more data when the scroll position is 90% from the top or bottom
+          style={{ overflow: 'hidden' }} // To prevent default scroll behavior
+        >
+          {years.map(year => (
+            <div key={year}>
+              <h2 className='tmdb-body-title'>{year}</h2>
+              <div className='tmdb-body-flex'>
+                {movies[year] && movies[year].sort((a, b) => b.popularity - a.popularity).map(item => (
+                  <div key={item.id} className='tmdb-body-flex-item'>
+                    <img className='tmdb-body-flex-item-img' src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.original_title} />
+                    <div className='tmdb-body-flex-item-copy'>
+                      <h3 className='tmdb-body-flex-item-copy-h3'>{item.original_title}</h3>
+                      <p className='tmdb-body-flex-item-copy-p'>{item.overview}</p>
+                    </div>
+                    <p className='tmdb-body-flex-item-copy-tag'>{item.vote_count}</p>
                   </div>
-                </div>
-              ))
-            )
-          }
-        </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   );
